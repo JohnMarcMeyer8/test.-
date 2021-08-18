150cc4c974c9b620338ef6edc9dea5045e2d11a4
JohnMarcMeyer8 <89034095+JohnMarcMeyer8@users.noreply.github.com>
2021-08-18 11:17:32 -0500

    Add files via upload

---
+++ Project2.cpp
@@ -0,0 +1,30 @@
+//name: John Meyer
+//date: 08/18/2021
+//class: Prof JD
+//description: Practice Problem 
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	float lengthOfARectangle;
+	float widthOfARectangle;
+	float perimeterOfARectangle;
+	float areaOfARectangle;
+
+
+	cout << "Give me total lengthOfARectangle ";
+	cin >> lengthOfARectangle;
+	cout << "Give me total widtOfARectangle ";
+	cin >> widthOfARectangle;
+	cout << "Give me total perimeterOfARectangle ";
+	cin >> perimeterOfARectangle;
+	cout << "Give me total areaOfARectangle ";
+	cin >> areaOfARectangle;
+
+	calculation = perimeterOfARectangle = 2 * lengthOfARectangle + widthOfARectangle;
+
+
+
+}
